8f34e5da1d63c8acb8a1486d04893d0429c4d42d
Andrew Garner <muzazzi@gmail.com>
2010-05-08 19:35:45 -0400

    adding dispatch support into amsterdam

---
+++ holland/core/dispatch/__init__.py
@@ -0,0 +1,10 @@
+"""Multi-consumer multi-producer dispatching mechanism
+
+Originally based on pydispatch (BSD) http://pypi.python.org/pypi/PyDispatcher/2.0.1
+See license.txt for original license.
+
+Dispatcher taken from Django-1.1 as a general event signalling mechanism for
+holland-1.1
+"""
+
+from holland.core.dispatch.dispatcher import Signal
