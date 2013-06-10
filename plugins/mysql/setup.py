7b42d3d5f80c384b95401798f9b65ea43000d9da
Andrew Garner <muzazzi@gmail.com>
2013-06-10 11:34:53 -0400

    add new base plugin directories
    This creates the location to merge the current plugin schema
    into holland-mysql, holland-lvm, holland-mysql-lvm and holland-pgsql
    plugins.

---
+++ plugins/mysql/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.1.0'
+
+setup(name='holland-mysql',
+      version=version,
+      description="",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Andrew Garner',
+      author_email='holland-coredev@lists.launchpad.net',
+      url='http://hollandbackup.org',
+      license='GPLv2',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+)
