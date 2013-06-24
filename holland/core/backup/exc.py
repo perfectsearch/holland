ab54311710378be0fc5161f7342dfb1cdabb6822
Andrew Garner <muzazzi@gmail.com>
2013-06-24 17:29:27 -0400

    make BackupError inherit from HollandError

--- holland/core/backup/exc.py
+++ holland/core/backup/exc.py
@@ -1,26 +1,14 @@
 """
-    holland.core.backup.error
-    ~~~~~~~~~~~~~~~~~~~~~~~~~
+holland.core.backup.exc
+~~~~~~~~~~~~~~~~~~~~~~~
 
-    Standard exceptions raised by the Holland Backup API
-
-    :copyright: 2010-2011 Rackspace US, Inc.
-    :license: BSD, see LICENSE.rst for details
+Backup exceptions
 """
 
-class BackupError(Exception):
-    """Raised when an error is encountered during a backup
+from holland.core.exc import HollandError
 
-    All BackupErrors should derive from this base class
-    """
+class BackupError(HollandError):
+    """Generic BackupError encountered"""
 
-    def __init__(self, message, chained_exc=None):
-        Exception.__init__(self, message)
-        self.message = message
-        self.chained_exc = chained_exc
-
-    def __str__(self):
-        msg = "%s" % self.message
-        if self.chained_exc:
-            msg += ": %s" % self.chained_exc
-        return msg
+class BackupHookError(HollandError):
+    """Error encountered while executing a backup hook"""
