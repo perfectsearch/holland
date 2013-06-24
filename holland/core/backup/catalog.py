2e578fdfad9b3398fc40c0fb266c0204aa21d8ac
Andrew Garner <muzazzi@gmail.com>
2013-06-24 17:51:17 -0400

    add initial catalog database support

---
+++ holland/core/backup/catalog.py
@@ -0,0 +1,124 @@
+from contextlib import contextmanager
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from holland.core.config import Config, ConfigError
+from . import models
+
+def load_catalog(url, *args, **kwargs):
+    """Create a session"""
+    if not url:
+        url = 'sqlite://'
+    if '//:' not in url:
+        url = 'sqlite:///' + url
+    engine = create_engine(url, *args, **kwargs)
+    models.DeclarativeBase.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    # initialize the catalog, if necessary here
+    session = Session()
+
+    try:
+        if not session.query(models.SchemaVersion).first():
+            session.add(models.SchemaVersion())
+    finally:
+        session.commit()
+
+    return Catalog(session)
+
+class Catalog(object):
+    def __init__(self, session):
+        self.session = session
+
+    def _query(self, model, *args, **kwargs):
+        """Query the backup catalog"""
+        query = self.session.query(model)
+        if args:
+            query = query.filter(*args)
+        if kwargs:
+            query = query.filter_by(**kwargs)
+        return query
+
+    @contextmanager
+    def save(self, model):
+        """Save a model to the catalog"""
+        session = self.session
+        session.add(model)
+        session.commit()
+        try:
+            yield model
+        except:
+            raise
+        finally:
+            session.commit()
+
+    def session(self):
+        return self.session_factory()
+
+    def load_backup(self, *args, **kwargs):
+        """Load a backup
+
+        Load the first matching backup that meets the filter criteria.
+
+        To load all the matching backups use ``list_backups`` instead.
+
+        :params args: args to filter backups by
+        :params kwargs: kwargs to filter backups by
+        :returns: ``Backup`` instance
+        """
+        query = self._query(models.Backup, *args, **kwargs)
+        return query.order_by(models.Backup.start_time).first()
+
+    def load_backup_from_node(self, node):
+        """Create a backup instance from a spool node
+        
+        :param node: BackupNode instance to examine
+        :returns: ``Backup`` model instance
+        """
+        backup = models.Backup(backup_directory=node.path)
+        try:
+            with node.open('.holland/status', 'rb') as statusf:
+                status = Config.from_iterable(statusf)
+        except (IOError, ConfigError):
+            backup.status = 'failed'
+        else:
+            backup.status = status.status
+        return backup
+
+    def previous_backup(self, backup):
+        """Load the first backup before the given backup
+
+        Queries the catalog database for a backup with a start time
+        preceding ``backup``
+
+        :param backup: backup instance to find previous backup of
+        :returns: ``Backup`` instance or None if no matching backup record
+                  found
+        """
+        query = self._query(models.Backup,
+                            models.Backup.start_time < backup.start_time)
+        return query.order_by(models.Backup.start_time.desc()).limit(1).first()
+
+    def next_backup(self, backup):
+        """Load the first backup after the given backup
+
+        :param backup: backup to base search on
+        :returns: ``Backup`` instance or None if no matching backup record
+        """
+        query = self._query(models.Backup,
+                            models.Backup.start_time > backup.start_time)
+        return query.order_by(models.Backup.start_time.desc()).limit(1).first()
+
+    def list_backups(self, *args, **kwargs):
+        """List all backups
+
+        :param args: args to filter backups by
+        :param kwargs: kwargs to filter backups by
+        :returns: list of ``Backup`` instances
+        """
+        query = self._query(models.Backup, *args, **kwargs)
+        return query.order_by(models.Backup.start_time).all()
+
+    def list_jobs(self):
+        query = self._query(models.Job, *args, **kwargs)
+        return query.order_by(models.Job.start_time).all()
+
---
+++ holland/core/backup/models.py
@@ -0,0 +1,177 @@
+"""
+holland.core.backup
+~~~~~~~~~~~~~~~~~~~
+
+Run backups through the holland backup manager
+
+:copyright: 2008-2011 Rackspace US, Inc.
+:license: BSD, see LICENSE.rst for details
+"""
+
+import os, sys
+import logging
+from contextlib import contextmanager
+from decimal import Decimal
+from datetime import datetime
+from subprocess import list2cmdline
+from sqlalchemy import create_engine, types
+from sqlalchemy.schema import Column, ForeignKey
+from sqlalchemy.types import String, Text, Integer, Numeric, DateTime
+from sqlalchemy.orm import backref, relation, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from holland.core import __version__ as __holland_version__
+
+LOG = logging.getLogger(__name__)
+
+class BaseExt(object):
+    """Does much nicer repr/print of class instances
+    from sqlalchemy list suggested by Michael Bayer
+    """
+    def __repr__(self):
+        attributes = []
+        for column in self.__table__.c:
+            name = column.name
+            value = getattr(self, name)
+            attributes.append('{name}={value!r}'.format(
+                                name=name, value=value
+                              )
+                             )
+
+        return "{cls}({attributes})".format(
+                    cls=self.__class__.__name__,
+                    attributes=', '.join(attributes)
+               )
+
+DeclarativeBase = declarative_base(cls=BaseExt)
+
+try:
+    from sqlalchemy.types import BigInteger
+except ImportError:
+    class BigInteger(Integer): pass
+    class BIGINT(BigInteger):
+        """SQL BigInt Type"""
+    class DBBigInteger(BigInteger):
+        def get_col_spec(self): return "BIGINT"
+    # monkey patch supported platforms
+    import sqlalchemy.databases.postgres
+    import sqlalchemy.databases.sqlite
+    import sqlalchemy.databases.mysql
+    sqlalchemy.databases.postgres.PGBigInteger = DBBigInteger
+    sqlalchemy.databases.postgres.colspecs[BigInteger] = DBBigInteger
+    sqlalchemy.databases.sqlite.SLBigInteger = DBBigInteger
+    sqlalchemy.databases.sqlite.colspecs[BigInteger] = DBBigInteger
+    class MSBigInteger(BigInteger, sqlalchemy.databases.mysql.MSInteger):
+        def __init__(self, display_width=None, **kw):
+            self.display_width = display_width
+            sqlalchemy.databases.mysql._NumericType.__init__(self, kw)
+            BigInteger.__init__(self, **kw)
+
+    def get_col_spec(self):
+        if self.display_width is not None:
+            return self._extend("BIGINT(%(display_width)s)" % {'display_width': self.display_width})
+        else:
+            return self._extend("BIGINT")
+
+    sqlalchemy.databases.mysql.MSBigInteger = MSBigInteger
+    sqlalchemy.databases.mysql.colspecs[BigInteger] = MSBigInteger
+
+class SchemaVersion(DeclarativeBase):
+    """Version table for the current model version in use
+
+    This is intended to be used for future upgrades, if needed
+    """
+    __tablename__ = 'schema_version'
+    __table_args__ = {'mysql_engine':'InnoDB'}
+    version = Column(Integer, primary_key=True)
+    holland_version = Column(String(128), default=__holland_version__)
+
+class Job(DeclarativeBase):
+    """Backup job consisting of one or more backups"""
+    __tablename__ = 'job'
+    __table_args__ = {'mysql_engine':'InnoDB'}
+
+    ## Database attributes
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    pid = Column(Integer, default=os.getpid)
+    cmdline = Column(Text, default=list2cmdline(sys.argv))
+    start_time = Column(DateTime, default=datetime.now)
+    stop_time = Column(DateTime)
+    status = Column(String, default='initialized')
+    external_id = Column(String(128), index=True)
+    is_dryrun = False
+
+    def __enter__(self):
+        self.start_time = datetime.now()
+        return self
+
+    def __exit__(self, exctype, exc, traceback):
+        self.stop_time = datetime.now()
+
+class Backup(DeclarativeBase):
+    """Backup run mapping to a single backup strategy"""
+    __tablename__ = 'backup'
+    __table_args__ = {'mysql_engine':'InnoDB'}
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    job_id = Column(Integer, ForeignKey("job.id"))
+    name = Column(String(255))
+    start_time = Column(DateTime, default=datetime.now)
+    stop_time = Column(DateTime)
+    message = Column(Text)
+    status = Column(String, default='initialized')
+    estimated_size = Column(BigInteger, default=None)
+    real_size = Column(BigInteger, default=None)
+    backup_directory = Column(String(4096))
+    config_path = Column(String(4096))
+    config = Column(Text)
+
+    job = relation("Job", backref=backref("backups", order_by=id))
+
+    def __enter__(self):
+        self.start_time = datetime.now()
+        return self
+
+    def __exit__(self, exctype, exc, traceback):
+        self.stop_time = datetime.now()
+
+    def duration(self):
+        start_time = self.start_time
+        stop_time = self.stop_time
+        if start_time and stop_time:
+            delta = stop_time - start_time
+            return (delta.days*24*60*60 + delta.seconds +
+                    delta.microseconds*10**-6)
+        else:
+            return None
+
+@contextmanager
+def timestamp_model(model):
+    """Set start_time/stop_time on a model
+
+    This is intended for use as a ContextManager
+    so that one can run:
+
+    with Backup() as backup:
+        # run a backup
+
+    And have the start/stop times automatically set.
+    """
+    model.start_time = datetime.now()
+    try:
+        yield model
+    finally:
+        model.stop_time = datetime.now()
+
+def create_session(*args, **kwargs):
+    """Create a session"""
+    engine = create_engine(*args, **kwargs)
+    DeclarativeBase.metadata.create_all(engine)
+    session = Session(bind=engine)
+
+    try:
+        if not session.query(SchemaVersion).first():
+            session.add(SchemaVersion())
+    finally:
+        session.commit()
+
+    return session
