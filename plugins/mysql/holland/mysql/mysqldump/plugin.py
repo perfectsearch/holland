11c04018e3a6d908b954250e42a5a0cd3d5ff3eb
Andrew Garner <muzazzi@gmail.com>
2013-07-31 15:12:25 -0400

    update holland.mysql.mysqldump plugin implementation

--- plugins/mysql/holland/mysql/mysqldump/plugin.py
+++ plugins/mysql/holland/mysql/mysqldump/plugin.py
@@ -1,133 +1,174 @@
-"""holland.mysql.mysqldump interface"""
+"""
+holland.mysql.mysqldump.plugin
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-import os, sys
+myqldump backup plugin implementation
+
+"""
+
+import os
+from os.path import join
+import time
 import logging
-from holland.core import BackupError, BackupPlugin, Configspec
-from holland.core.util import parse_bytes
-from holland.mysql.mysqldump.mock import MockEnvironment
-from holland.mysql.mysqldump.util import *
-from holland.mysql.mysqldump.runner import MySQLBackup
-from holland.mysql.mysqldump.spec import CONFIGSPEC
+import contextlib
+from tempfile import TemporaryFile
+from subprocess import check_call, CalledProcessError
+from holland.core import BackupPlugin, load_stream_plugin
+from holland.core.util import human_duration, human_size, relpath
+from holland.mysql.schema import InformationSchema
+from holland.mysql.client import MySQL, generate_defaults_file
+from . import util, filters, views, strategy
 
 LOG = logging.getLogger(__name__)
 
-class MySQLDumpPlugin(BackupPlugin):
-    """Backup Plugin for MySQL using mysqldump"""
+class MySQLDumpBackupPlugin(BackupPlugin):
+    name = 'mysqldump'
+    summary = 'mysqldump backups'
 
-    #: internal schema object where the mysqldump plugin tracks and caches
-    #: metadata about tables for a MySQL instance
-    _schema = None
-
-    #: A cached MySQLClient instance used for retrieving basic information
-    #: from a MySQL instance including metadata, replication info, etc.
-    _client = None
-
-    def pre(self):
-        """Setup objects shared by estimate() and backup/dryrun()"""
-        self._schema = schema_from_config(self.config['mysqldump'])
-        self._client = client_from_config(self.config['mysql:client'])
-        try:
-            self._client.connect()
-            LOG.info(" + Connected to MySQL")
-        except MySQLError, exc:
-            LOG.error(" + Connection to MySQL failed")
-            raise BackupError("[%d] %s" % exc.args)
-        log_host_info(self._client)
-        try:
-            LOG.info(" + Evaluating schema")
-            refresh_schema(self._schema, self._client)
-        except MySQLError, exc:
-            raise BackupError("[%d] %s" % exc.args)
+    def setup(self):
+        self.defaults_file = join(self.backup_directory, 'holland-mysqldump.cnf')
+        client_cfg = self.config['mysql:client']
+        include_paths = client_cfg.defaults_file
+        generate_defaults_file(self.defaults_file, include_paths, **client_cfg)
+        LOG.info("Generated defaults file: %s", self.defaults_file)
+        for name in include_paths:
+            LOG.info("Added !include %s to defaults", os.path.expanduser(name))
+        self.mysql = MySQL.from_defaults(self.defaults_file)
+        self.ischema = InformationSchema(self.mysql)
+        LOG.info("Applying schema filters")
+        database_exclusions = self.config.mysqldump.exclude_databases
+        if 'information_schema' not in database_exclusions:
+            LOG.info("Adding database exclusion for information_schema")
+            database_exclusions.append('information_schema')
+        if 'performance_schema' not in database_exclusions:
+            LOG.info("Adding database exclusion for performance_schema")
+            database_exclusions.append('performance_schema')
+        filters.apply_filters(self.ischema, self.config.mysqldump)
 
     def estimate(self):
-        """Estimate the size of a mysqldump from MySQL metadata
-
-        :returns: estimated size in integer number of bytes
-        """
-        LOG.info("Estimating backup size")
-        LOG.info("----------------------")
-        if self.config['mysqldump']['estimate-method'].startswith('const:'):
-            return parse_bytes(self.config['mysqldump']['estimate-method'])
-        return sum([db.size for db in self._schema.databases])
-
-    def _setup(self):
-        """Perform various setup bookkeeping"""
-        os.makedirs(os.path.join(self.backup_directory, 'backup_data'))
-        LOG.info("+ mkdir %s", os.path.join(self.backup_directory,
-                                            'backup_data'))
-        defaults_file = defaults_from_config(self.config['mysql:client'],
-                                             self.backup_directory)
-        LOG.info("+ mkconfig %s", defaults_file)
-        write_exclusions(defaults_file, self._schema)
-        LOG.info("+ exclusions >> %s", defaults_file)
-        mysqld_version = server_version(self._client)
-        argv = argv_from_config(defaults_file, self.config, mysqld_version)
-        dotsql_generator = sql_open(os.path.join(self.backup_directory,
-                                                 'backup_data'),
-                                    self.config['compression'])
-        lock_method = lock_method_from_config(self.config)
-        if lock_method:
-            LOG.info("+ lock-method forced : %s", lock_method)
-        return MySQLBackup(argv, dotsql_generator, lock_method)
-
-    def backup(self):
-        """Backup via mysqldump"""
-        LOG.info("mysqldump backup")
-        LOG.info("----------------")
-        LOG.info(":databases: %s",
-                 ','.join([db.name + (db.excluded and '(excluded)' or '')
-                          for db in self._schema.databases]))
+        total_size = 0
         try:
-            try:
-                backup = self._setup()
-                config = self.config['mysqldump']
-                if config['stop-slave']:
-                    status = stop_slave(self._client)
-                    record_slave_status(status, self.config)
-                if config['lockless-only']:
-                    check_transactional(self._schema.databases)
-                if config['file-per-database']:
-                    parallelism = config['parallelism']
-                    explicit_tables = config['explicit-tables']
-                    LOG.info("+ file-per-database")
-                    LOG.info("+ parallelism=%d", parallelism)
-                    generate_manifest(self.backup_directory,
-                                      self._schema,
-                                      self.config)
-                    backup.run_each(self._schema.databases,
-                                    explicit_tables=explicit_tables,
-                                    parallelism=parallelism)
-                else:
-                    backup.run_all(self._schema.databases)
-            except (KeyboardInterrupt, SystemExit):
-                raise
-            except:
-                LOG.debug("Failure(exception)", exc_info=True)
-                raise BackupError("Backup failed", sys.exc_info()[1])
-        finally:
-            if self.config['mysqldump']['stop-slave']:
-                start_slave(self._client)
+            for schema in self.ischema.databases():
+                schema_size = self.ischema.data_size(schema)
+                total_size += schema_size
+                LOG.info("Estimated size for `%s`: %s", schema, human_size(schema_size))
+        except self.mysql.DatabaseError, exc:
+            LOG.error("MySQL error during estimation: [%d] %s", *exc.args)
+            self.fail("MySQL error during estimation: [%d] %s" %
+                       exc.args)
+        return total_size
+
+    @contextlib.contextmanager
+    def mysqldump_context(self):
+        config = self.config.mysqldump
+        managers = []
+        if config.stop_slave:
+            managers.append(self.mysql.pause_replication())
+        #if config.read-lock-during-dump:
+        #    managers.append(self.mysql.lock_instance)
+
+        with contextlib.nested(*managers):
+            #record_replication(self.backup_directory,
+            #                   slave_status=self.mysql.show_slave_status(),
+            #                   master_status=elf.mysql.show_master_status())
+            yield
 
     def dryrun(self):
-        """Perform a dryrun backup"""
-        mockenv = MockEnvironment()
-        mockenv.replace_environment()
+        LOG.info("Dry-run for 'mysqldump'")
+        config = self.config.mysqldump
+        bin_mysqldump = util.which_mysqldump(config.mysql_binpath)
+        mysqldump_version = util.mysqldump_version(bin_mysqldump)
+        if config.exclude_invalid_views:
+            LOG.info("Would check for invalid views")
+        util.generate_mysqldump_options(self.defaults_file, config, mysqldump_version)
+        compression = load_stream_plugin(self.config.compression)
+        LOG.info("Compression method '%s'", compression.name)
+        if config.file_per_database:
+            LOG.info("file-per-database enabled. Forcing mysqldump-strategy=file-per-database")
+            config.mysqldump_strategy = 'file-per-database'
+        LOG.info("mysqldump-strategy: %s", config.mysqldump_strategy)
+        s = strategy.load_strategy(config.mysqldump_strategy)
+        mysqldump_cmd = strategy.mysqldump(bin_mysqldump,
+                                           self.defaults_file,
+                                           config.additional_options)
+        if config.stop_slave:
+            LOG.info("Would pause replication via STOP SLAVE SQL_THREAD")
+        for basename, cmdline in s(mysqldump_cmd, self.ischema, config):
+            if compression.ext:
+                basename += compression.ext
+            LOG.info("$ %s", ' '.join(cmdline))
+            LOG.info("  > backup_data/%s", basename)
+        if config.stop_slave:
+            LOG.info("Would resume replication via START SLAVE SQL_THREAD")
+
+    def backup(self):
+        config = self.config.mysqldump
+        defaults_file = self.defaults_file
+        bin_mysqldump = util.which_mysqldump(config.mysql_binpath)
+        mysqldump_version = util.mysqldump_version(bin_mysqldump)
+        util.check_version(mysqldump_version, server_version=self.mysql.version)
+        backup_datadir = join(self.backup_directory, 'backup_data')
         try:
-            self.backup()
-        finally:
-            mockenv.restore_environment()
+            os.makedirs(backup_datadir)
+        except OSError, exc:
+            if exc.errno != errno.EEXIST:
+                self.fail("Unable to create %s: [%d] %s" % (
+                            backup_datadir,
+                            exc.errno,
+                            exc.strerror))
 
-    def configspec(self):
-        """Generate the configspec for the mysqldump plugin"""
-        return Configspec.from_string(CONFIGSPEC)
+        util.generate_mysqldump_options(defaults_file, config, mysqldump_version)
+        util.generate_table_exclusions(defaults_file, self.ischema)
+        if config.exclude_invalid_views:
+            invalid_views_path = join(backup_datadir, 'invalid_views.sql')
+            views.dump_and_exclude_invalid_views(invalid_views_path,
+                                                 defaults_file,
+                                                 self.ischema)
 
-    def plugin_info(self):
-        """Provide information about this plugin"""
-        return dict(
-            name='mysqldump',
-            summary='Backup MySQL databases via the mysqldump command',
-            description='''
-            ''',
-            version='1.1.0a1',
-            api_version='1.1.0a1',
-        )
+        compression = load_stream_plugin(self.config.compression)
+
+        if config.file_per_database:
+            LOG.info("file-per-database enabled. Forcing mysqldump-strategy=file-per-database")
+            config.mysqldump_strategy = 'file-per-database'
+        LOG.info("mysqldump-strategy: %s", config.mysqldump_strategy)
+        s = strategy.load_strategy(config.mysqldump_strategy)
+        mysqldump_cmd = strategy.mysqldump(bin_mysqldump,
+                                           defaults_file,
+                                           config.additional_options)
+        # handle any stop/start slave or other global
+        # contextual behavior under which we will run the backup
+        with self.mysqldump_context():
+            for basename, cmdline in s(mysqldump_cmd, self.ischema, config):
+                path = join(self.backup_directory, 'backup_data', basename)
+                if not os.path.exists(os.path.dirname(path)):
+                    os.makedirs(os.path.dirname(path))
+                with compression.open(path, 'wb') as stdout:
+                    with TemporaryFile(prefix='mysqldump') as stderr:
+                        # XXX: list2cmdline or sarge.shell_quote
+                        # may be more accurate here
+                        LOG.info("$ %s ", ' '.join(cmdline))
+                        LOG.info(" > %s", relpath(stdout.name,
+                                                  self.backup_directory))
+                        start_time = time.time()
+                        try:
+                            check_call(cmdline,
+                                       stdout=stdout,
+                                       stderr=stderr,
+                                       close_fds=True)
+                            LOG.info("%s dumped in %s",
+                                     relpath(stdout.name,
+                                             self.backup_directory),
+                                     human_duration(time.time() - start_time))
+                        except OSError, exc:
+                            LOG.info("[%d] %s", exc.errno, exc.strerror)
+                            self.fail("Failed to run mysqldump [%d] %s" %
+                                    (exc.errno, exc.strerror))
+                        except CalledProcessError, exc:
+                            stderr.seek(0)
+                            stderr.flush()
+                            for line in stderr:
+                                LOG.error("mysqldump: %s", line.rstrip())
+                            LOG.info("mysqldump failed with status [%d]",
+                                     exc.returncode)
+                            self.fail("mysqldump failed with status [%d]" %
+                                      exc.returncode)
