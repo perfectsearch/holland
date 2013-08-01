f2f3832218b29bf18b9675934a0164e2c983efb4
Andrew Garner <muzazzi@gmail.com>
2013-07-31 23:30:55 -0400

    cleanup lvm's setup.py

--- plugins/lvm/setup.py
+++ plugins/lvm/setup.py
@@ -3,24 +3,26 @@ import sys, os
 
 version = '2.0.0'
 
-setup(name='holland.lvm',
-      version=version,
-      description="LVM snapshot backup support for holland",
-      long_description="""\
+setup(
+name='holland.lvm',
+version=version,
+description="LVM snapshot backup support for holland",
+long_description="""\
 """,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='Andrew Garner',
-      author_email='holland-coredev@lists.launchpad.net',
-      url='http://hollandbackup.org',
-      license='GPLv2',
-      packages=find_packages(exclude=['tests', 'tests.*']),
-      include_package_data=True,
-      zip_safe=True,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
+classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+keywords='',
+author='Andrew Garner',
+author_email='holland-coredev@lists.launchpad.net',
+url='http://hollandbackup.org',
+license='GPLv2',
+packages=find_packages(exclude=['tests', 'tests.*']),
+include_package_data=True,
+zip_safe=True,
+install_requires=[
+# -*- Extra requirements: -*-
+],
+entry_points="""
+[holland.backup]
+lvmsnapshot = holland.lvm.plugin:LVMSnapshot
+""",
 )
