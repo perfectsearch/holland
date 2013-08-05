42f13800f8b8813675db62a78234a87c9887e7e5
Andrew Garner <muzazzi@gmail.com>
2013-08-05 16:16:02 -0400

    cleanup holland/__init__.py
    - flesh out docstring
    - remove old __version__ information

--- holland/__init__.py
+++ holland/__init__.py
@@ -1,6 +1,14 @@
-"Holland Backup"
+"""
+holland
+~~~~~~~
 
-__version__ = '1.1.0a2'
-__version_info__ = (1, 1, 0, 'a', 2)
+Holland backup manager
 
+This is the base namespace for the holland backup manager
+
+This is namespace package.  The core holland implemtation uses
+the holland.core namespace and the holland command line interface
+uses the holland.cli namespace.  Holland plugins implemented by
+the Holland Core development team will exist under holland.{plugin_name}.
+"""
 __import__('pkg_resources').declare_namespace(__name__)
