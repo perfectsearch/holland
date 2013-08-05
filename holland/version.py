b940be6d31d50a997d98795428a797657f0901c8
Andrew Garner <muzazzi@gmail.com>
2013-08-05 16:18:40 -0400

    update holland.version
    - add __version_info__ a tuple of the __version__ string
    - add docstring

--- holland/version.py
+++ holland/version.py
@@ -1,1 +1,17 @@
-__version__ = '2.0.1'
+"""
+holland.version
+~~~~~~~~~~~~~~~
+
+Holland version information
+
+This module includes two variables:
+
+__version__ - a string version of the current holland release
+__version_info__ - __version__ as a tuple
+
+This is used by setup.py to set the version as well as internally
+by various holland utilities that report the holland version.
+"""
+
+__version__ = '2.0.0'
+__version_info__ = tuple([int(part) for part in __version__.split('.')])
